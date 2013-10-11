bc8fc56371d0c60f79bfe408a0c5445b140f81f4
kikunantoka <kikunantoka@gmail.com>
2013-10-11 15:48:10 +0900

    plus and minus button added

--- edison/app/views/new.blade.php
+++ edison/app/views/new.blade.php
@@ -7,21 +7,23 @@
   <h1>投稿する</h1>
   {{ Form::open(array('url' => '/new')) }}
     <div class="form-group">
-      {{ Form::label('title', 'title') }}
+      <h3>{{ Form::label('title', 'title') }}</h3>
       {{ Form::text('title', '', array('class' => 'form-control', 'id' => 'title', 'placeholder' => 'Enter title')) }}
     </div>
     <div class="form-group">
-      {{ Form::label('content', 'content') }}
+      <h3>{{ Form::label('content', 'content') }}</h3>
       {{ Form::textarea('content', '', array('class' => 'form-control', 'rows' => '5', 'id' => 'content', 'placeholder' => 'Enter content')) }}
     </div>
-    Category
+    <h3>Category</h3>
     <select class="form-control" name="category_id">
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
       <option value="4">4</option>
     </select>
-    Tags
+    <h3>Tags</h3>
+    <div class="btn btn-default"><span class="glyphicon glyphicon-plus"></span></div>
+    <div class="btn btn-default"><span class="glyphicon glyphicon-minus"></span></div>
     {{ Form::text('tags[]', '', array('class' => 'form-control')) }}
     {{ Form::text('tags[]', '', array('class' => 'form-control')) }}
     <br />
