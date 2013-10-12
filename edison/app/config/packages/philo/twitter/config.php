4f321bb15a1aa34a03bc02d4ee37ee0b119a30d8
Tomohiro Suwa <neoen.gsn@gmail.com>
2013-10-12 14:48:53 +0900

    change keys

--- edison/app/config/packages/philo/twitter/config.php
+++ edison/app/config/packages/philo/twitter/config.php
@@ -1,8 +1,6 @@
 <?php
 
 return array(
-    // 'CONSUMER_KEY'    => '0V19YV9uUlIVbAFXCAuBQ',
-    // 'CONSUMER_SECRET' => 'Q4Ea2luCCULvkt2lpfw8IBnVzW1heHKtnac4CTVcI'
-  'CONSUMER_KEY'    => '50OCZ2Cwd3vMazCS6G4QEA',
-  'CONSUMER_SECRET' => 'kWSu4APSBPJxxUdqOxUIwZWdI5cqVNnrMrLLpQng'
+  'CONSUMER_KEY'    => 'USgomm87iAAXQWpcZgzhg',
+  'CONSUMER_SECRET' => 'rlEJSJJAG1v510gFWw4xbq9TF2zV8msha1I6q5ZB5Y'
 );
