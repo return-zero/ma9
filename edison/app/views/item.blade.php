9f7f1b98783dedda25144de701892239de560be8
super-wakame <t.nagai1212@gmail.com>
2013-10-12 17:47:10 +0900

    edit html of /{screen_name}/items/{id}!

--- edison/app/views/item.blade.php
+++ edison/app/views/item.blade.php
@@ -1,16 +1,51 @@
 @extends('layouts.base')
 @section('header')
-  <title>item</title>
+<title>item</title>
 @stop
 @section('content')
-<div class="container hero-unit">
-  <h1>Item</h1>
-  <p>title: {{ $title }}</p>
-  <p>content: {{ $content }}</p>
-  <p>created_at: {{ $created_at }}</p>
-  <p>updated_at: {{ $updated_at }}</p>
-  @foreach ($tags as $tag)
-    <p>tags: {{ $tag }}</p>
-  @endforeach
+<h1>{{ $title }}</h1>
+
+<div class="row">
+	<div class="col-lg-9">
+		<p>作成日 : {{ $created_at }}</p>
+		<p>更新日	 : {{ $updated_at }}</p>			
+	</div>
+	<div class="col-lg-3">
+		<div class="profile">
+			profile
+		</div>
+	</div>
 </div>
+
+<div class="row">
+	<div class="col-lg-12">
+		<div class="col-lg-2">
+			タグ
+		</div>
+		<div class="col-lg-10">
+			@foreach ($tags as $tag)
+				<div class="tag">{{ $tag }}</div>
+			@endforeach
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-12">
+		content: {{ $content }}
+	</div>	
+</div>
+
+<div class="row">
+	<h2>投稿作品</h2>
+	<div class="job">
+	</div>
+</div>
+
+<div class="row">
+	<h3>コメント</h3>
+	<div class="comment">
+	</div>
+</div>
+
 @stop
