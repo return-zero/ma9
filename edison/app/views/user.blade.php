bdea65d548e70e1e19f926e0e4d0a4e660cd718a
kikunantoka <kikunantoka@gmail.com>
2013-10-14 03:06:36 +0900

    modified user page

--- edison/app/views/user.blade.php
+++ edison/app/views/user.blade.php
@@ -12,21 +12,30 @@
 @section('content')
 <h1>{{ $screen_name }} さんのページ</h1>
 <div class="row">
-	<div class="col-lg-9">
+	<div class="col-lg-9" style="background:skyblue;">
 	  @foreach ($items as $item)
 	    <h3><a href="/{{ $screen_name }}/items/{{ $item['item_id'] }}">{{ $item['title'] }}</a></h3>
 	  @endforeach
 	</div>
-	<div class="col-lg-3">
-		<p>NAME: {{ $screen_name }}</p>
+	<div class="col-lg-3" style="background:blue;">
+		<img src="{{ $icon }}">
+		<div class="row">
+			<div class="col-lg-3" style="background:yellow;">
+				<p>NAME:</p>
+			</div>
+			<div class="col-lg-9" style="background:orange;">
+				<p>{{ $name }}</p>
+			</div>
+		</div>
+		
 	</div>
 </div>
 <!-- /.row -->
 <hr>
 <p>screen_name: {{ $screen_name }}</p>
-<p>name: {{-- $name --}}</p>
-<p>description: {{-- $desc --}}</p>
-<img src="{{-- $icon --}}">
+<p>name: {{ $name }}</p>
+<p>description: {{ $desc }}</p>
+<img src="{{ $icon }}">
 @foreach ($items as $item)
 	<p>items: {{ $item['item_id'] }}</p>
   <p>items: {{ $item['title'] }}</p>
