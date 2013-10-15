54ac192ab04d7a791722beea0164f97a24aacce5
Masaaki Morishita <ikasama3.0@gmail.com>
2013-10-15 16:41:58 +0900

    version up jquery(1.10.1)

--- edison/app/views/layouts/base.blade.php
+++ edison/app/views/layouts/base.blade.php
@@ -39,9 +39,9 @@
       <h4>ma9</h4>
     </footer>
     @section('js')
-    {{ HTML::script('http://code.jquery.com/jquery-1.9.1.min.js') }}
+    {{ HTML::script('http://code.jquery.com/jquery-1.10.1.min.js') }}
     {{ HTML::script('http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js') }}
-    {{ HTML::script('js/script.js') }}
+    {{ HTML::script('js/app.min.js') }}
     @show
   </body>
 </html>
