8031aa5c61edf1c8e945e6bbc0093c283f11119b
Masaaki Morishita <ikasama3.0@gmail.com>
2013-10-21 21:49:40 +0900

    create works table

---
+++ edison/app/database/migrations/2013_10_21_214648_create_works_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('works', function($table) {
+			$table->increments('id');
+			$table->integer('item_id');
+			$table->string('url');
+			$table->datetime('created_at');
+			$table->datetime('updated_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('works');
+	}
+
+}
