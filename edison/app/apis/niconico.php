1fb49979c0a28b5cfad6cfc7bbee0d37f7bc1bc9
Tomohiro Suwa <neoen.gsn@gmail.com>
2013-10-24 00:39:45 +0900

    implement niconico api

---
+++ edison/app/apis/niconico.php
@@ -0,0 +1,41 @@
+<?php
+
+class NicoSugoiSearch {
+  const NICONICO_SEARCH_URL = 'http://api.search.nicovideo.jp/api/';
+
+  public function __construct() {
+  }
+
+  public function search($service, $query) {
+    $post_data = array(
+      'query' => $query,
+      'service' => array($service),
+      'search' => array('title'),
+      'join' => array('cmsid', 'title', 'description', 'thumbnail_url', 'view_counter', 'comment_counter', 'mylist_counter'),
+      'filters' => array(array(
+        'type' => 'equal',
+        'field' => 'ss_adult',
+        'value' => false
+      )),
+      'from' => 0,
+      'size' => 5,
+      'sort_by' => 'view_counter',
+      'issuer' => 'apiguide',
+      'reason' => 'ma9'
+    );
+
+    $data = json_encode($post_data);
+    $headers = array(
+      'Content-type: application/json',
+      'Accept: application/json'
+    );
+    $curl = curl_init(self::NICONICO_SEARCH_URL);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $ret = curl_exec($curl);
+    $ret = explode("\n", $ret);
+    curl_close($curl);
+    return json_decode($ret[2]);
+  }
+}
--- edison/app/controllers/ItemController.php
+++ edison/app/controllers/ItemController.php
@@ -5,13 +5,13 @@ class ItemController extends BaseController {
   public function showItem($screen_name, $id)
   {
     $user = DB::table('users')->where('screen_name', '=', $screen_name)->get();
-    $data = DB::table('items')->where('id', '=', $id)->where('user_id', '=', $user[0]->id)->get();
-    $tagmaps = DB::table('tagmaps')->where('item_id', $data[0]->id)->get();
+    $item = DB::table('items')->where('id', '=', $id)->where('user_id', '=', $user[0]->id)->get();
+    $tagmaps = DB::table('tagmaps')->where('item_id', $item[0]->id)->get();
     $tags = array();
     foreach ($tagmaps as $tagmap) {
       $tags[] = DB::table('tags')->select('content')->where('id', $tagmap->tag_id)->get()[0]->content;
     }
-    
+
     $comments = array();
     $cs = Comment::where('item_id', '=', $id)->get();
     foreach ($cs as $comment) {
@@ -19,25 +19,44 @@ class ItemController extends BaseController {
       $comment['name'] = $name;
       $comments[] = $comment;
     }
-    
+
     $star_status = false;
     if (Auth::check()) {
-    	$auth_id = Auth::user()->id;
+      $auth_id = Auth::user()->id;
       $star_status = $this->getStarStatus($auth_id,$id);
     }
 
-    $item = array(
-      'id' => $data[0]->id,
-      'title' => $data[0]->title,
-      'content' => $data[0]->content,
-      'created_at' => $data[0]->created_at,
-      'updated_at' => $data[0]->updated_at,
+    $nico = new NicoSugoiSearch();
+    $query = implode(' | ', $tags);
+    $ret = $nico->search($item->type, $query);
+    $related_works = array();
+    if (isset($ret->values)) {
+      foreach ($ret->values as $value) {
+        $related_works[] = array(
+          'cmsid' => $value->cmsid,
+          'title' => $value->title,
+          'thumbnail_url' => $value->thumbnail_url,
+          'view_counter' => $value->view_counter,
+          'comment_counter' => $value->comment_counter,
+          'mylist_counter' => $value->mylist_counter
+        );
+      }
+    }
+
+    $data = array(
+      'id' => $item[0]->id,
+      'title' => $item[0]->title,
+      'content' => $item[0]->content,
+      'created_at' => $item[0]->created_at,
+      'updated_at' => $item[0]->updated_at,
       'comments' => $comments,
       'tags' => $tags,
       'screen_name' => $screen_name,
       'star_status' => $star_status,
+      'related_works' => $related_works
     );
-    return View::make('item', $item);
+
+    return View::make('item', $data);
   }
 
   public function delete($screen_name, $id) {
@@ -56,7 +75,7 @@ class ItemController extends BaseController {
     $user = User::where('screen_name', '=', $screen_name)->first();    
 
     $comment = new Comment;
-    
+
     $comment->user_id = Auth::user()->id;
     $comment->item_id = $id;
     $comment->comment = $data['comment'];
@@ -78,19 +97,19 @@ class ItemController extends BaseController {
       return Redirect::to("/$screen_name/item/$id");      
     }
   }
-  
+
   public function getUserIdByScreenName($screen_name) {
     $user = User::where('screen_name', '=', $screen_name)->first();
     return $user->id;
   }
-  
+
   /* ----------------------
      Star
-     ---------------------- */
+  ---------------------- */
 
   public function star($screen_name, $id) {
     $now = date('Y-m-d H:i:s');
-    
+
     DB::table('starmaps')->insert(
       array(
         'item_id' => $id,
@@ -103,7 +122,7 @@ class ItemController extends BaseController {
   }
 
   public function unstar($screen_name, $item_id) {
-  	$user_id = $this->getUserIdByScreenName($screen_name);
+    $user_id = $this->getUserIdByScreenName($screen_name);
     $starmap = Starmap::where('user_id', '=', $user_id)->where('item_id', '=', $item_id)->first();
     if (Auth::user()->id === $starmap->user_id) {
       Starmap::destroy($starmap->id);
@@ -122,13 +141,13 @@ class ItemController extends BaseController {
     );
     return View::make('stargazers', $data);
   }
-  
+
   private function getStarStatus($auth_id, $item_id) {
     if(Starmap::where('user_id', '=', $auth_id)->where('item_id', '=', $item_id)->first() != NULL) { 
-	    return true;
+      return true;
     }
     return false;
   }
-  
-  
+
+
 }
