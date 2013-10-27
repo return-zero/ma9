c03aeb032c5413597000dc12e4bbb9a77f62db92
kikunantoka <kikunantoka@gmail.com>
2013-10-28 07:07:20 +0900

    Merge

--- edison/app/views/item.blade.php
+++ edison/app/views/item.blade.php
@@ -70,10 +70,11 @@
                 
                
                   <div class="col-lg-10">
-                    <div class="item-title">
-                      <p><a href="{{ $work->item_poster_screen_name }}/items/{{ $work->item_id }}">{{ $work->title }}</a></p>
+                    <div class="work-title">
+                      <p>{{ $work->title }}</p>
                     </div>
                     <p><a href="{{ $work->screen_name }}">{{ $work->screen_name }}</a> が投稿しました</p>
+                    <p>{{ $work->comment }}</p>
                   </div>
                 
                 
