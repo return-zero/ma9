10ff085c55f68bfd9180c2b0fc76883fe5a8627c
Masaaki Morishita <ikasama3.0@gmail.com>
2013-10-29 07:20:55 +0900

    before fix

--- edison/app/controllers/ApiController.php
+++ edison/app/controllers/ApiController.php
@@ -47,28 +47,52 @@ class ApiController extends BaseController {
     }
 
     $notice_items_uesr = array();
-
     $notice_item_titles = array();
+    $notice_work_ids = array();
     foreach ($notice_item_ids as $notice_item_id) {
       $notice_items_uesr_ids[] = Item::where('id', '=', $notice_item_id)->get()[0]['user_id'];
       $notice_item_titles[] = Item::where('id', '=', $notice_item_id)->get()[0]['title'];
+      $notice_work_ids[] = Work::where('item_id', '=', $notice_item_id)->orderBy('created_at', 'desc')->get()[0]['item_id'];
     }
+    //$this->debug($notice_work_ids);
+
+    $notice_work_user_ids = array();
+    $notice_work_title = array();
+    foreach ($notice_work_ids as $notice_work_id) {
+      var_dump($notice_work_id);
+      $notice_work_user_ids[] = Work::where('id', '=', $notice_work_id)->orderBy('updated_at', 'asc')->get()[0]['user_id'];
+      $notice_work_title[] = Work::where('id', '=', $notice_work_id)->orderBy('id', 'asc')->get()[0]['title'];
+    }
+    $this->debug($notice_work_user_ids);
+    //$this->debug($notice_work_title);
+// ---------
+
+    $notice_work_user_screen_name = array();
+    foreach ($notice_work_user_ids as $notice_work_user_id) {
+      $notice_work_user_screen_name = User::where('id', '=', $notice_work_user_id)->get()[0]['screen_name'];
+    }
+    //$this->debug($notice_work_user_screen_name);
+
 
     $notice_item_screen_name = array();
     foreach ($notice_items_uesr_ids as $user_id) {
       $notice_item_screen_name[] = User::where('id', '=', $user_id)->get()[0]['screen_name'];
     }
 
+
+
     $json_val = array("notice_title" => $notice_item_titles,
                       "notice_item_id" => $notice_item_ids,
-                      "notice_item_user" => $notice_item_screen_name);
+                      "notice_item_user" => $notice_item_screen_name,
+                      "notice_work_title" => $notice_work_title,
+                      "notice_work_user" => $notice_work_user_screen_name);
 
     header('Content-type: application/json');
     echo json_encode($json_val);
   }
 
 
-  public function debug($val)
+  public static function debug($val)
   {
     echo "<pre>";
     var_dump($val);
